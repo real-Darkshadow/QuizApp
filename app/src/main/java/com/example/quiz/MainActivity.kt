1d6354351a2496001fbfcc0c7fac43eece908da0
real-Darkshadow <aadarsh.21bcan262@jecrcu.edu.in>
2022-11-08 18:22:59 +0530

    Welcome Page functionality

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import com.example.quiz.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_FULLSCREEN
+        binding.startbtn.setOnClickListener{
+            val intent:Intent=Intent(this,questions()::class.java)
+            intent.putExtra("name",binding.textField.editText?.text.toString())
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/quiz/questions.kt
+++ app/src/main/java/com/example/quiz/questions.kt
@@ -1,11 +1,18 @@
 package com.example.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quiz.databinding.ActivityQuestionsBinding
 
 class questions : AppCompatActivity() {
+    private lateinit var binidng:ActivityQuestionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_questions)
+        val binding=ActivityQuestionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val name=intent.getStringExtra("name").toString()
+        binding.t2.text=name
+
     }
 }
